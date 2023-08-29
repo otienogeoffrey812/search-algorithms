a75009a3bd533fc21879dbdac5637c897ab8fefb
Geoffrey Otieno <otienogeoffrey812@gmail.com>
2023-08-29 21:34:52 +0300

    Ternary search algorithm

--- src/main/java/org/example/Search.java
+++ src/main/java/org/example/Search.java
@@ -19,7 +19,6 @@ public class Search {
     }
 
     public int binarySearchIterative(int[] array, int target){
-
         int left = 0;
         int right = array.length - 1;
 
@@ -34,5 +33,34 @@ public class Search {
             }
         }
         return  -1;
+    }
+
+    public int ternarySearch(int[] array, int target) {
+        return ternarySearch(array, target, 0, array.length - 1);
+    }
+
+    private int ternarySearch(
+            int[] array, int target, int left, int right) {
+
+        if (left > right)
+            return -1;
+
+        int partitionSize = (right - left) / 3;
+        int mid1 = left + partitionSize;
+        int mid2 = right - partitionSize;
+
+        if (array[mid1] == target)
+            return mid1;
+
+        if (array[mid2] == target)
+            return mid2;
+
+        if (target < array[mid1])
+            return ternarySearch(array, target, left, mid1 - 1);
+
+        if (target > array[mid2])
+            return ternarySearch(array, target, mid2 + 1, right);
+
+        return ternarySearch(array, target, mid1 + 1, mid2 - 1);
     }
 }

3ed3052177e155c3f978c6090591af7a1c372f09
Geoffrey Otieno <otienogeoffrey812@gmail.com>
2023-08-29 21:45:36 +0300

    Jump search algorithm

--- src/main/java/org/example/Search.java
+++ src/main/java/org/example/Search.java
@@ -34,14 +34,10 @@ public class Search {
         }
         return  -1;
     }
-
     public int ternarySearch(int[] array, int target) {
         return ternarySearch(array, target, 0, array.length - 1);
     }
-
-    private int ternarySearch(
-            int[] array, int target, int left, int right) {
-
+    private int ternarySearch(int[] array, int target, int left, int right) {
         if (left > right)
             return -1;
 
@@ -62,5 +58,23 @@ public class Search {
             return ternarySearch(array, target, mid2 + 1, right);
 
         return ternarySearch(array, target, mid1 + 1, mid2 - 1);
+    }
+    public int jumpSearch(int[] array, int target) {
+        int blockSize = (int) Math.sqrt(array.length);
+        int start = 0;
+        int next = blockSize;
+
+        while (start < array.length &&
+                array[next - 1] < target) {
+            start = next;
+            next += blockSize;
+            if (next > array.length)
+                next = array.length;
+        }
+
+        for (var i = start; i < next; i++)
+            if (array[i] == target)
+                return i;
+        return -1;
     }
 }
